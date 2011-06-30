02326288365c584401961379b2547c3cf57f84d6
Christopher <c.freden@gmail.com>
2011-06-30 19:35:54 +0200

    Created functions for converting between user model and database model

--- geobin.js
+++ geobin.js
@@ -15,18 +15,35 @@ var conf = {
   ,new mongodb.Server(conf.dbHost, conf.dbPort)
   ,{native_parser: true})
 ,ObjectID = db.bson_serializer.ObjectID
-,getCollection = function (name, callback) {
+,getCollection = function (name, fn) {
   if (db.state === 'notConnected') {
     db.open(function (err, p_client) {
-      db.collection(name, callback);
+      db.collection(name, fn);
     });
   } else {
-    db.collection(name, callback);
+    db.collection(name, fn);
   }
+}
+,geoToDoc = function (geo) {
+  if (typeof geo.category !== 'string') throw 'geo.category must be a string';
+  return {
+    loc: [geo.lon, geo.lat]
+    ,cat: geo.category
+    ,data: {msg: geo.msg}
+  };
+}
+,docToGeo = function (doc) {
+  return {
+    id: doc._id
+    ,lon: doc.loc[0]
+    ,lat: doc.loc[1]
+    ,msg: doc.data.msg
+    ,category: doc.category
+  };
 }
 
 ,server = http.createServer()
-,endChain = function (err, req, res, callback) {
+,endChain = function (err, req, res, fn) {
   if (err) {
     console.log('Error occured: '+err);
     res.writeHead(500);
@@ -34,19 +51,19 @@ var conf = {
     return;
   }
   res.end();
-  callback(err, req, res);
+  if (fn) fn(err, req, res);
 }
-,textChain = function (err, req, res, callback, data) {
+,textChain = function (err, req, res, fn, data) {
   res.setHeader('Content-Type', 'text/plain');
   res.write(data);
-  callback(err, req, res);
+  if (fn) fn(err, req, res);
 }
-,jsonChain = function (err, req, res, callback, data) {
+,jsonChain = function (err, req, res, fn, data) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   if (data) {
     data = JSON.stringify(data);
     var query = parseUrl(req.url, true).query;
-    if (query.jsonp) {
+    if (query.jsonp && req.method === 'GET') {
       res.setHeader('Content-Type', 'application/javascript');
       data = query.jsonp+'('+data+');';
     } else {
@@ -54,17 +71,17 @@ var conf = {
     }
   }
   res.write(data);
-  callback(err, req, res);
+  if (fn) fn(err, req, res);
 }
 ,dispatch = {
-  loc: function (req, res) {
+  geo: function (req, res) {
     ({
       GET: function () {
         getCollection('location', function (err, collection) {
           var parts = parseUrl(req.url).pathname.split('/');
           console.log('DEBUG: Finding location ID: '+parts[2]);
           collection.find({_id:new ObjectID(parts[2])}).nextObject(function (err, doc) {
-            jsonChain(err, req, res, endChain, doc);
+            jsonChain(err, req, res, endChain, docToGeo(doc));
           });
         });
       }
@@ -73,20 +90,14 @@ var conf = {
         req.on('data', function (chunk) { data += chunk; });
         req.on('end', function() {
           console.log('DEBUG: Data from POST recived: '+data);
-          try {
-            var loc = JSON.parse(data);
-            getCollection('location', function (err, collection) {
-              collection.insert({
-                loc: [loc.lon, loc.lat]
-                ,data: {msg: loc.msg}
-              }
-              ,function (err, docs) {
-                endChain(err, req, res);
-              });
+          var doc = geoToDoc(JSON.parse(data));
+          console.log('DEBUG: Inserting into database:');
+          console.dir(doc);
+          getCollection('location', function (err, collection) {
+            collection.insert(doc, function (err, docs) {
+              jsonChain(err, req, res, endChain, docToGeo(docs[0]));
             });
-          } catch (err) {
-            endChain(err, req, res);
-          }
+          });
         });
       }
     })[req.method]();
@@ -118,7 +129,7 @@ var conf = {
 
 server.on('request', function (req, res) {
   var parts = parseUrl(req.url).pathname.split('/');
-  console.log('DEBUG: Incoming request: '+req.url);
+  console.log('DEBUG: Incoming '+req.method+' request: '+req.url);
   try {
     if (req.method === 'OPTIONS') {
       res.writeHead(200, {
